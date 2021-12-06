6e3d4ef5c6c2b762973f25a621db14fa88fdaba7
ranitbcd <ranit.98mondal@gmail.com>
2021-12-06 17:44:58 +0530

    api discover

---
+++ src/app/interface.ts
@@ -0,0 +1,7 @@
+export interface iface{
+    uname:string,
+    city:string,
+    contact_no:number,
+    trade:string,
+    sem:number
+}
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import {HttpClient,HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { iface } from './interface';
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
 
   constructor(private _http: HttpClient) { }
-  searchuser(body:any){
-    return this._http.post('http://localhost:7071/api/apitest', body,{
-      observe:'body'
-    });
+  // searchuser(body:any){
+  //   return this._http.post('http://localhost:7071/api/apitest', body,{
+  //     observe:'body'
+  //   });
+  // }
+  getuser():Observable<iface>{
+    return this._http.get<iface>('http://localhost:7071/api/demoapi');
   }
+
 
 }
